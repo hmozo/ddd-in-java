d4d2177e7d0fda4a63d07604249f1b6f52be8468
nacikonojeras <nacikonojeras@192.168.2.105>
2020-12-18 19:32:17 +0100

    Config

---
+++ ddd-in-java/src/main/java/tv/codely/apps/DddInJavaApplication.java
@@ -0,0 +1,13 @@
+package tv.codely.apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddInJavaApplication.class, args);
+	}
+
+}
